d570b4af68bfa961bb5487fbe88561853d46de6a
Anthony Mikhaiel <c3037092@newcastle.ac.uk>
2025-06-24 20:35:51 +0300

    Pass parking lots of selected city to js

--- booking/views.py
+++ booking/views.py
@@ -23,13 +23,25 @@ def book():
 @booking_bp.route('/city_selected', methods=['POST'])
 def city_selected():
     data = request.get_json()
-    city = data.get('city')
-    print("City selected:", city)
-    return jsonify({'status': 'ok'})
+    cityName = data.get('city')
+    print("City selected:", cityName)
+    city = City.query.filter_by(city=cityName).first()
+    parkingLots = ParkingLot.query.filter_by(city_id=city.id).all()
+    for lot in parkingLots:
+        print(lot.address)
+    return jsonify([{'id': lot.id, 'name': lot.address} for lot in parkingLots])
+
 
 @booking_bp.route('/parking_lot', methods=['GET', 'POST'])
 def parking_lot():
     data = request.get_json()
     city_id = data.get('city')
+    lots = ParkingLot.query.filter_by(city_id=city_id).all()
+    return jsonify([{'id': lot.id, 'name': lot.name} for lot in lots])
+
+
+@booking_bp.route('/get_parking_lots')
+def get_parking_lots():
+    city_id = request.args.get('city_id')
     lots = ParkingLot.query.filter_by(city_id=city_id).all()
     return jsonify([{'id': lot.id, 'name': lot.name} for lot in lots])
