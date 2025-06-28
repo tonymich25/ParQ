d4a9173c03d2a02e862ad7d25a8fb3551cf771e7
Anthony Mikhaiel <c3037092@newcastle.ac.uk>
2025-06-28 19:17:33 +0300

    test front end

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,11 @@
 from flask import render_template, Blueprint
+from flask_login import login_required
 
 dashboard_bp = Blueprint('dashboard', __name__, template_folder='dashboard')
 
 
 @dashboard_bp.route('/dashboard', methods=['GET', 'POST'])
-#@login_required
+@login_required
 def dashboard():
 
 
