4ee28209a6aa35bc64285d713acbf4e93534e93b
Anthony Mikhaiel <c3037092@newcastle.ac.uk>
2025-07-22 21:48:53 +0200

    Added error handling and error pages (base1 needs ui fix for final prod)

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from config import app
 from flask import render_template
+import errors
+
+app.register_error_handler(400, errors.bad_request)
+app.register_error_handler(401, errors.unauthorized)
+app.register_error_handler(403, errors.forbidden)
+app.register_error_handler(404, errors.not_found)
+app.register_error_handler(405, errors.method_not_allowed)
+app.register_error_handler(408, errors.request_timeout)
+app.register_error_handler(500, errors.internal_server_error)
+app.register_error_handler(502, errors.bad_gateway)
+app.register_error_handler(503, errors.service_unavailable)
+app.register_error_handler(504, errors.gateway_timeout)
 
 @app.route('/')
 def index():
